9d00f6fca54010c26e556ee29b3227cc8c1cc0ab
Shreyas Moolya <shreyasmooli007@gmail.com>
2023-11-15 12:34:37 +0530

    Completed level 2 of project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App(){
     return (<div>
         <Header />
-        <Note 
-            title="This is a title"
-            content="This is content"
-        />
+        {notes.map(noteItem => (
+            <Note 
+                key={noteItem.key}
+                title={noteItem.title}
+                content={noteItem.content}
+            />
+            ))}
         <Footer />
     </div>
     );
